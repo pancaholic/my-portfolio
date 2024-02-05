6d7a441cb620f344131fc056a8e36d427e8392e4
Putra Panca <putrapanca21@gmail.com>
2024-02-05 15:05:46 +0700

    fixing vercel issues pt3

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -31,10 +31,10 @@ const EmailSection = () => {
     <section id="scrollTarget" className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
         <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-700 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-2 transform -translate-x-1/2 -translate-1/2"></div>
         <div className="z-10">
-            <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+            <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
             <p className="text-[#ADB7BE] mb-4 max-w-md">
                 {" "}
-                I'm currently looking for new opportunities, feel free to send me a message
+                I&apos;m currently looking for new opportunities, feel free to send me a message
                 my inbox is always open. I try my best to get back at you!
             </p>
             <p className="text-[#ADB7BE] mb-4 max-w-md font-bold">
