39f923e2476d8d24540d712e097b83e86efba512
Putra Panca <putrapanca21@gmail.com>
2024-02-12 08:56:07 +0700

    bug-fixes-3

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,9 +3,13 @@ import React, {forwardRef} from 'react';
 const Footer = () => {
   return (
     <footer className= "footer border z-10 border-t-[#33353f] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
+      <div className="container p-12 flex justify-center sm:justify-between flex-wrap text-center sm:text-left">
         <span>PORTFOLIO</span>
-        <p className="text-slate-600">© 2023 Putra Panca Prasetya. All Rights Reserved.</p>
+        <p className="text-slate-600">
+          
+          © 2023 Putra Panca Prasetya. All Rights Reserved.
+          
+        </p>
       </div>
     </footer>
   )
